36ab274a5d50be8d14dd51565a7c58d05c1a59ab
smitagaikwad25 <gaikwadsmita021@gmail.com>
2022-03-02 18:25:44 +0530

    Added: swagger for login and register api

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ dotenv.config();
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../src/swagger/swagger.json'
 
 import routes from './routes';
 import database from './config/database';
@@ -26,6 +28,9 @@ app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan('combined', { stream: logStream }));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 database();
 
